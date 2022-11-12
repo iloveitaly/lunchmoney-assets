16bbb57163bb6d7bccbdbe08b401e5ab4346da76
Michael Bianco <iloveitaly@gmail.com>
2022-11-12 16:01:58 +0000

    Try cd to fix execution issues in the docker container

--- cron.sh
+++ cron.sh
@@ -10,7 +10,7 @@ if [ "${SCHEDULE}" = "NONE" ]; then
 else
   # `-l` ensures that /etc/profile is picked up by the sh subshell
   # stdout redirect ensures logs are redirected to the parent process https://snippets.aktagon.com/snippets/945-how-to-get-cron-to-log-to-stdout-under-docker-and-kubernetes
-  echo "$SCHEDULE root sh -lc '/usr/local/bin/node $PWD/dist/index.js' > /proc/1/fd/1 2>&1" >> /etc/crontab
+  echo "$SCHEDULE root sh -lc 'cd $PWD && /usr/local/bin/node $PWD/dist/index.js' > /proc/1/fd/1 2>&1" >> /etc/crontab
 
   # debian slim does not include cron by default, this must be installed via Dockerfile
   # additionally, busybox does *not* add the cron process, so we must use the debian cron package
